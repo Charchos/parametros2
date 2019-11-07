009c397556fd638f22ab41b80eadd5eb4eb74cdf
Alumno <Alumno@PC26>
2019-11-07 12:25:09 -0600

    Practica 8, Actividad 1

---
+++ operadores logicos/operadores logicos/operadores logicos.cpp
@@ -0,0 +1,27 @@
+// operadores logicos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+	float x, y;
+	printf("Ingrese el valor de x: ");
+	scanf("%f",&x);
+	if (x< 0 || x >50) 
+	{
+		y = 0;
+	}
+	else if (x<= 10) 
+	{
+		y = 4 / x;
+	}
+	else if (x <= 25)
+	{
+		y = pow(x, 3) + pow(x, 3) - 18;
+	}
+	printf("\n\n x= %5.2f \t y= %5.2f", x, y);
+	return 0;
+
+}
